270753d84c983a7b815c2d2604484fdae35bd1de
Aryan1706 <aryanrajputsfe@gmail.com>
2023-06-21 21:43:06 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header/header";
+import Hero from "./components/Hero/hero";
 
 function App() {
   return (
-    <div className="App"><Header/></div>
+    <div className="App">
+      <Header/>
+      <Hero/>
+    </div>
   );
 }
 
